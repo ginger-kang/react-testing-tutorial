1f579f321fef548e07a8ba106eb60ca5eb015ace
ginger-kang <kdhoon07@gmail.com>
2021-08-03 23:01:41 +0900

    Paragraph and image testing using screen

--- src/NotFound.test.js
+++ src/NotFound.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import NotFound from "./NotFound";
 
 describe("<NotFound />", () => {
@@ -7,5 +7,22 @@ describe("<NotFound />", () => {
     const { getByText } = render(<NotFound path="/hello" />);
     const header = getByText("Page Not Found");
     expect(header).toBeInTheDocument();
+  });
+
+  it("renders paragraph", () => {
+    render(<NotFound path="/hello" />);
+    const paragraph = screen.getByText(/해당 페이지/i);
+    expect(paragraph).toHaveTextContent(
+      "해당 페이지(/hello)를 찾을 수 없습니다.",
+    );
+  });
+
+  it("renders image", () => {
+    render(<NotFound path="/hello" />);
+    const image = screen.getByAltText("404");
+    expect(image).toHaveAttribute(
+      "src",
+      "https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif",
+    );
   });
 });
